28e3e102ed0dbf81c1fad6e5ab071ae8bb1fe3f1
art3mis145 <art3mis145@gmail.com>
2022-11-27 15:33:35 +0700

    1/2

--- src/app/pages/post/components/create-post/create-post.component.ts
+++ src/app/pages/post/components/create-post/create-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 import { ProfileUser } from 'src/app/models/user-profile';
 import { PostService } from 'src/app/services/post.service';
 @Component({
@@ -10,6 +11,7 @@ import { PostService } from 'src/app/services/post.service';
 })
 export class CreatePostComponent implements OnInit {
   user!: ProfileUser;
+  imageSrc: string[] = [];
   constructor(
     private postService: PostService,
     @Inject(MAT_DIALOG_DATA) public data: ProfileUser,
@@ -31,5 +33,16 @@ export class CreatePostComponent implements OnInit {
     await this.postService.postMessage(message, this.data);
     form.resetForm();
     this.dialogRef.close();
+  }
+  readURL(event: any): void {
+    if (event.target.files) {
+      for (let index = 0; index < File.length; index++) {
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[index]);
+        reader.onload = (events: any) => {
+          this.imageSrc.push(events.target.result as string);
+        };
+      }
+    }
   }
 }
